e7e53ef540812701239d656d21d29579af949ae4
SEHUN PARK <j48750@daum.net>
2022-03-12 01:45:28 +0900

    first commit

---
+++ 001_IO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("학번을 입력하세요 : ");
+            string id = Console.ReadLine();
+            int result = int.Parse(id);
+
+            Console.WriteLine(result);
+        }
+    }
+}
