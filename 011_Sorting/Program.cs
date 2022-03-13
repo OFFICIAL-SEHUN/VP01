87096203e5046050ca70fa93679c84df6992d6ac
SEHUN PARK <j48750@daum.net>
2022-03-13 16:17:56 +0900

    3

--- 011_Sorting/Program.cs
+++ 011_Sorting/Program.cs
@@ -9,7 +9,29 @@ namespace _011_Sorting
     internal class Program
     {
         static void Main(string[] args)
-        {   
-        }
+        {
+            Random r = new Random();
+            int[] sort = new int[10];
+            for (int i = 0; i < 10; i++) sort[i] = r.Next(100); //랜덤값 저장
+            int n = 10;
+            int tmp;
+            for (int i = 0; i < n; i++) // i회전
+            {
+                for (int j = 0; j < n - (i + 1); j++) //순회할 원소의 개수만큼 loop
+                                                              /*조건이 [ j < n - (i + 1) ]인 이유:
+                                                              버블정렬 특성상 1회전마다 가장 큰 수는 정렬이 되어있게 된다.*/
+                                                              /* 1회전마다 탐색할 원소의 개수 n(배열 개수) - i(회전 수) 이다.
+                                                                 i + 1 인 이유는 j = 0 부터 시작하기 때문이다. */
+                {
+                    if (sort[j] > sort[j + 1])
+                    {
+                        tmp = sort[j + 1];
+                        sort[j + 1] = sort[j];
+                        sort[j] = tmp;
+                    }
+                }
+            }
+            for (int i = 0; i < sort.Length; i++) Console.Write("{0}  ", sort[i]);
+            Console.WriteLine();        }
     }
 }
--- 012_Larger/Program.cs
+++ 012_Larger/Program.cs
@@ -10,6 +10,21 @@ namespace _012_Larger
     {
         static void Main(string[] args)
         {
+            int Larger(int a, int b, int c)
+            {
+                int bigger;
+                bigger = (a > b) ? a : b;
+                bigger = bigger > c ? bigger : c;
+                return bigger;
+            }
+            Console.Write("3개의 정수를 입력하세요 : ");
+            string str_num = Console.ReadLine();
+            string[] result = str_num.Split(new char[] { ' ' });  // new char[] {} 로 해도 공백으로 나눈다는 의미 
+            for (int i = 0; i < result.Length; i++)               // char 인 이유: split 함수는 '문자'를 입력 받음. 문자열을 문자로 바꿈
+            {
+                Console.Write(result[i]);
+            }
+            Console.WriteLine(Larger(result[0],result[1],result[2])); <int로 변환
         }
     }
 }
