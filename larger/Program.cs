264e7ada56672b879e7025a553e355ff8bc9c528
OFFICIAL-SEHUN <j48750@daum.net>
2022-03-16 14:07:10 +0900

    3.16

---
+++ larger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int Larger(int n1,int n2)
+            {
+                return n1 > n2 ? n1 : n2;
+            }
+            Console.Write("숫자 입력 :");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("숫자 입력 :");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.Write("숫자 입력 :");
+            int n3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Larger(Larger(n1,n2),n3));
+
+        }
+    }
+}
