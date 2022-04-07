ce0cfef1f6769225c86b46d84662d8959a95ff94
OFFICIAL-SEHUN <j48750@daum.net>
2022-04-07 15:55:05 +0900

    4/7

--- 033_StackPannel/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ 033_StackPannel/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -109,7 +109,7 @@ namespace _033_StackPannel {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/033_StackPannel;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/033_StackPannel;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
---
+++ 036_ChessBoard/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _036_ChessBoard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            chess_Board.Rows = 8;
+            chess_Board.Columns = 8;
+            
+            for (int k = 1; k < 9; k++) //줄별로
+            {
+                
+                if (k % 2 == 0)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Rectangle r1 = new Rectangle();
+                            r1.Fill = Brushes.Black;
+                            chess_Board.Children.Add(r1);
+                        }
+                        else
+                        {
+                            Rectangle r1 = new Rectangle();
+                            r1.Fill = Brushes.Red;
+                            chess_Board.Children.Add(r1);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            Rectangle r1 = new Rectangle();
+                            r1.Fill = Brushes.Red;
+                            chess_Board.Children.Add(r1);
+                        }
+                        else
+                        {
+                            Rectangle r1 = new Rectangle();
+                            r1.Fill = Brushes.Black;
+                            chess_Board.Children.Add(r1);
+                        }
+                    }
+                }
+
+            }
+            
+            
+        }
+    }
+}
